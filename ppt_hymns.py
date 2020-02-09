9bc7e41a731082ae6aa05059167e4c42d87759a8
Daniel Li <daniel.li317@gmail.com>
2020-02-09 15:34:15 -0500

    updating for windows

--- ppt_hymns.py
+++ ppt_hymns.py
@@ -14,7 +14,7 @@ def get_hymnal_url(title):
         if 'hymnary' in result:
             return result
 
-def get_hymn_lyrics(site, hymn_number=None, url=None, file_name=None):
+def get_hymn_lyrics(site, hymn_number=None, url=None, file_name_chinese=None, file_name_english=None):
     if site == 'christian study':
         if (hymn_number < 100) & (hymn_number >= 10):
             hymn_number = f'0{hymn_number}'
@@ -33,7 +33,7 @@ def get_hymn_lyrics(site, hymn_number=None, url=None, file_name=None):
             lyrics = lyrics.replace('<font size="1">', '').replace('</font>', '').replace('<font size="+2">', '')
             lyrics = lyrics.replace('[', '').replace(']', '').replace(',', '')
             if lyrics is not None:
-                with open(file_name, 'a', encoding='utf-8') as f:
+                with open(file_name_chinese, 'a', encoding='big5') as f:
                     f.write(lyrics)
     else:
         response = requests.get(url)
@@ -46,7 +46,7 @@ def get_hymn_lyrics(site, hymn_number=None, url=None, file_name=None):
                     lyrics = lyrics.replace('<br/>', '')
                     lyrics = lyrics.replace('<p>', '').replace('</p>', '')
                     if lyrics is not None:
-                        with open(file_name, 'a') as f:
+                        with open(file_name_english, 'a') as f:
                             f.write(lyrics)
                             f.write('\n')
             except:
@@ -70,35 +70,26 @@ def get_hymn_titles(url=f'http://www.christianstudy.com/lifehymns.html'):
                 pass
     return hymn_titles
 
-class color:
-   PURPLE = '\033[95m'
-   CYAN = '\033[96m'
-   DARKCYAN = '\033[36m'
-   BLUE = '\033[94m'
-   GREEN = '\033[92m'
-   YELLOW = '\033[93m'
-   RED = '\033[91m'
-   BOLD = '\033[1m'
-   UNDERLINE = '\033[4m'
-   END = '\033[0m'
-
 def main(hymn_numbers):
-    file_name = f'lyrics_for_{hymn_numbers.replace(",", "_")}.txt'
-    with open(file_name, 'w') as f:
+    file_name_chinese = f'lyrics_for_{hymn_numbers.replace(",", "_")}_chinese.txt'
+    file_name_english = f'lyrics_for_{hymn_numbers.replace(",", "_")}_english.txt'
+    with open(file_name_chinese, 'w') as f:
+        f.write('Lyrics for Hymns\n')
+    with open(file_name_english, 'w') as f:
         f.write('Lyrics for Hymns\n')
     hymn_titles = get_hymn_titles()
     hymn_numbers = hymn_numbers.split(',')
     hymn_numbers = [int(x) for x in hymn_numbers]
     for hymn_number in hymn_numbers:
-        get_hymn_lyrics(site='christian study', hymn_number=hymn_number, file_name=file_name)
+        get_hymn_lyrics(site='christian study', hymn_number=hymn_number, file_name_chinese=file_name_chinese)
 
     for hymn_number in hymn_numbers:
-        with open(file_name, 'a') as f:
+        with open(file_name_english, 'a') as f:
             f.write('\n')
             f.write(hymn_titles[hymn_number])
             f.write('\n')
         url = get_hymnal_url(title=hymn_titles[hymn_number] + ' hymn')
-        get_hymn_lyrics(site='hymnal', url=url, file_name=file_name)
+        get_hymn_lyrics(site='hymnal', url=url, file_name_english=file_name_english)
 
 
 if __name__ == '__main__':
